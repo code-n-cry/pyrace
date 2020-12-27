7ec04cb4f33efe6330276ab7c16c438dd6f7ff7f
makszhuravlev <55282505+makszhuravlev@users.noreply.github.com>
2020-12-27 17:38:37 +0300

    Delete mobs.py

--- ГОНКИ/menu_and_game/mobs.py
+++
@@ -1,7 +0,0 @@
-import pygame
-import os
-
-
-class Player(pygame.sprite.Sprite):
-    image = pygame.image.load('\\'.join(os.getcwd().split('\\')[:-1]) + '\\menu_and_game\\game_data\\Car1.png')
-    image.set_colorkey((255, 255, 255))
